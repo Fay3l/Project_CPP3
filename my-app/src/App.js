4a5790ac865bc6fc922537e25fbd81fe103ea635
Fay3l <bonboui12@gmail.com>
2023-09-01 11:50:12 +0200

    second commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -82,7 +82,7 @@ function App() {
 const login = () => {setIsLoggedIn(true);connect()}
   
 const connect = () =>{
-  const newWs = new WebSocket("ws://192.168.1.53:8000/products");
+  const newWs = new WebSocket("ws://backend:8000/products");
   setWs(newWs);
 
   newWs.onopen = () => {
